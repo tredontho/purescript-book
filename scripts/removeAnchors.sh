180c155c10e4a2a9ae07fc1acae050e20105eacd
Wiktor Żurawik <wiktor.zurawik@gmail.com>
2023-07-10 23:53:32 +0200

    [Scripts] Make removeAnchors script support Darwin's BSD find (#444)
    * [Scripts] Make removeAnchors script support Darwin's BSD find
    
    ---------
    
    Co-authored-by: Miles Frain <milesfrain@users.noreply.github.com>

--- scripts/removeAnchors.sh
+++ scripts/removeAnchors.sh
@@ -8,7 +8,14 @@ set -x
 set -e
 
 # All .purs & .js files in the src/ and test/ directories of chapter exercises.
-FILES=$(find . -regextype posix-extended -regex '\./exercises/chapter[0-9]{1,2}/(src|test)/.*\.(purs|js)' -type f)
+FIND_FILES_PATTERN='\./exercises/chapter[0-9]{1,2}/(src|test)/.*\.(purs|js)'
+
+EXTENDED_REGEX_FLAGS="-regextype posix-extended"
+# BSD find has different flags for extended regex
+if [[ $(uname) == 'FreeBSD' || $(uname) == 'Darwin' ]]; then
+    EXTENDED_REGEX_FLAGS='-E'
+fi
+FILES=$(find . -type f $EXTENDED_REGEX_FLAGS -regex $FIND_FILES_PATTERN)
 
 for f in $FILES; do
   # Delete lines starting with an 'ANCHOR' comment
